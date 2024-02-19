712baef01300d1042ecca1b1c9d0cd5d518acb04
emreakay <emreakay@users.noreply.github.com>
2024-02-19 20:46:24 +0000

    Fix styling

--- src/Contracts/IssueActorModelContract.php
+++ src/Contracts/IssueActorModelContract.php
@@ -11,15 +11,15 @@ interface IssueActorModelContract
      * returns the available actor connection types for an issue model actor
      * example: issue_responsible, issue_participant, issue_observer
      * issue connections : responsible_model, participant_model, issue_model
+     *
      * @return array<string>
      */
-    public static function getActorConnectiveConnectionTypes() : array;
+    public static function getActorConnectiveConnectionTypes(): array;
 
-    
     public function getActorName(): string;
 
     /**
      * ConnectiveCollection<AIssue>
      */
-    public function getActingModels(string $connectionType) : ConnectiveCollection;
+    public function getActingModels(string $connectionType): ConnectiveCollection;
 }
