059491e2f57c2302c3611e9701ae8a234c1c5f76
emreakay <emreakay@users.noreply.github.com>
2024-02-19 13:18:25 +0000

    Fix styling

--- src/Models/AIssue.php
+++ src/Models/AIssue.php
@@ -17,36 +17,21 @@ class AIssue extends Model
 
     protected $table = 'aissue_issues';
 
-    /**
-     * @return string
-     */
     public function getIssueType(): string
     {
         return $this->issue_type;
     }
 
-    /**
-     * @param  string  $status
-     * @return bool
-     */
     public function canMakeTransition(string $status): bool
     {
         return \AuroraWebSoftware\AIssue\Facades\AIssue::canMakeTransition($this, $status);
     }
 
-    /**
-     * @param  string  $status
-     * @return AIssue
-     */
     public function makeTransition(string $status): AIssue
     {
         return \AuroraWebSoftware\AIssue\Facades\AIssue::makeTransition($this, $status);
     }
 
-    /**
-     * @param  AIssue  $issue
-     * @return array
-     */
     public function getTransitionableStatuses(AIssue $issue): array
     {
         return \AuroraWebSoftware\AIssue\Facades\AIssue::getTransitionableStatuses($this);
