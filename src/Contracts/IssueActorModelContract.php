3fba0cd94386b534c6110dd10171c68ac9b38042
emreakay <emreakay@yandex.com>
2024-02-19 20:45:04 +0000

    some

---
+++ src/Contracts/IssueActorModelContract.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AuroraWebSoftware\AIssue\Contracts;
+
+/**
+ * IssueActorModelContract
+ */
+interface IssueActorModelContract
+{
+    /**
+     * returns the available actor connection types for an issue model actor
+     * example: issue_responsible, issue_participant, issue_observer
+     * issue connections : responsible_model, participant_model, issue_model
+     * @return array<string>
+     */
+    public static function getActorConnectiveConnectionTypes() : array;
+
+    
+    public function getActorName(): string;
+
+    /**
+     * ConnectiveCollection<AIssue>
+     */
+    public function getActingModels(string $connectionType) : ConnectiveCollection;
+}
